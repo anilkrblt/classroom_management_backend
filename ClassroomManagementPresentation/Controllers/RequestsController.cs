2c55ace58aed3a74cc43f8bf313a94b03216fbaa
Fırat Can <can_6240@hotmail.com>
2024-12-21 14:30:05 +0300

    Request Controller.

--- ClassroomManagementPresentation/Controllers/RequestsController.cs
+++ ClassroomManagementPresentation/Controllers/RequestsController.cs
@@ -6,44 +6,67 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Service.Contracts;
+using Shared.DataTransferObjects;
 
 
 
 namespace ClassroomManagementPresentation.Controllers
 {
     [ApiController]
-    [Route("api/requests")]
+    [Route("api/room")]
     public class RequestsController : ControllerBase
     {
 
         private readonly IServiceManager _servisManager;
 
-        public RequestsController(ILogger<RequestsController> logger, IServiceManager serviceManager)
+        public RequestsController(IServiceManager serviceManager)
         {
             _servisManager = serviceManager;
         }
 
 
 
-        [HttpGet()]
+        [HttpGet("/requests")]
         public IActionResult GetAllRequests()
         {
             var requests=_servisManager.RequestService.GetAllRequestsAsync(false);
             return Ok(requests);
         }
 
-        [HttpGet("request/{id:int}")]
-        public async Task<IActionResult> GetRequestsByRoomId([FromRoute(Name ="id")]int roomId)
+        [HttpGet("/{id:int}/request")]
+        public IActionResult GetRequestsByRoomId([FromRoute(Name ="id")]int roomId)
         {
-            var request=await _servisManager.RequestService.GetRequestsByRoomId(roomId,false,false);
+            var request=  _servisManager.RequestService.GetRequestsByRoomId(roomId,false,false);
             return Ok(request);
         }
 
+        [HttpPut("/request/{requestId:int}/update")]
+        public IActionResult UpdateRequest(int requestId,[FromBody] RequestDto requestDto)
+        {
+            var request=_servisManager.RequestService.UpdateRequestAsync(requestId,requestDto,true);
+            return Ok(request);
+        }
+
+        [HttpPost("{roomId:int}/request/create")]
+        public async Task<IActionResult>CreateRequestForInstructor([FromBody]RequestDto requestDto,[FromBody]int instructorId,int roomId)
+        {
+            var request=await _servisManager.RequestService.CreateRequestForRoomByInstructorIdAsync(requestDto,instructorId,roomId,true);
+            return Ok(request);
+        }
+        [HttpPost("{roomId:int}/request/create")]
+        public async Task<IActionResult>CreateRequestForStudent([FromBody]RequestDto requestDto,[FromBody]int studentId,int roomId)
+        {
+            var request=await _servisManager.RequestService.CreateRequestForRoomByStudentIdAsync(requestDto,studentId,roomId,true);
+            return Ok(request);
+        }
+        [HttpDelete("{roomId:int}/request/delete")]
+        public async Task<IActionResult> DeleteRequest([FromRoute(Name ="id")]int roomId,[FromBody]int requestId)
+        {
+            _servisManager.RequestService.DeleteRequest(roomId,requestId,true,true);
+            return NoContent();
+        }
+
         /*
-        Route	Method
-        /api/requets	GET
-        /api/requets/:roomId	GET   /api/rooms/:id/requests daha mantıklı oluyor
-        /api/request/update/:id	PUT   
         /api/request/:roomId/create-request	POST    /api/rooms/:id/create-request
         /api/request/update/:id	DELETE
         */
